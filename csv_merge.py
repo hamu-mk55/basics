96dc8996e73cafafdb0da6132ca8998b24445041
hamu-mk55 <153355786+hamu-mk55@users.noreply.github.com>
2023-12-09 20:51:27 +0900

    Add files via upload

--- csv_merge.py
+++ csv_merge.py
@@ -3,11 +3,12 @@ import csv
 import os
 
 
-def csv_merge(data_dir:str='./data',
-              merge_file:str='merge.csv'
-              )->None:
+def csv_merge(data_dir: str = './data',
+              merge_file: str = 'merge.csv'
+              ) -> None:
     """
     data_dirの中のCSVファイルをマージして出力する。
+    マージファイルには、元ファイルのホルダ名/ファイル名も出力する。
     :param data_dir: データホルダ名
     :param merge_file: マージファイル名
     :return:
@@ -18,27 +19,28 @@ def csv_merge(data_dir:str='./data',
         os.remove(merge_file)
 
     # ファイル探索
-    filenames=glob.glob(f'{data_dir}/**/*.csv', recursive=True)
+    filenames = glob.glob(f'{data_dir}/**/*.csv', recursive=True)
     for file_cnt, filename in enumerate(filenames):
         print(filename)
         _dir = os.path.dirname(filename)
         _file = os.path.basename(filename)
 
         fr = open(filename, "r")
-        log=csv.reader(fr,delimiter=',',lineterminator='\n')
+        log = csv.reader(fr, delimiter=',', lineterminator='\n')
 
-        fw = open(merge_file ,'a')
-        csvWriter = csv.writer(fw,lineterminator='\n')
+        fw = open(merge_file, 'a')
+        csvWriter = csv.writer(fw, lineterminator='\n')
         for row_cnt, row in enumerate(log):
+            # 最初のファイルの場合、ヘッダを出力
+            if file_cnt == 0 and row_cnt == 0:
+                row = ['dir_name', 'file_name'] + row
+                csvWriter.writerow(row)
 
-            if file_cnt==0 and row_cnt==0:
-                row=['dir_name', 'file_name']+row
+            # データ出力
+            elif row_cnt > 0:
+                row = [_dir, _file] + row
                 csvWriter.writerow(row)
-            
-            elif row_cnt>0:
-                row=[_dir, _file]+row
-                csvWriter.writerow(row)
-                
+
         fw.close()
         fr.close()
 
