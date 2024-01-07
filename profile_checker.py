b4cc81f2bd222f6f178b4f17067d68ea5ded7ea0
hamu-mk55 <153355786+hamu-mk55@users.noreply.github.com>
2024-01-07 18:46:45 +0900

    Add files via upload

---
+++ profile_checker.py
@@ -0,0 +1,30 @@
+from line_profiler import LineProfiler
+
+from blob_analysis import example, blob_analysis
+
+
+def exam_func1(input=10):
+    _ret = exam_func2(input, repeat_num=10000)
+
+    return _ret
+
+
+def exam_func2(input=10, repeat_num=1000):
+    for _ in range(repeat_num):
+        _ret = input + 10
+    return _ret
+
+
+def check_profiler():
+    prof = LineProfiler()
+    prof.add_function(exam_func1)
+    prof.add_function(exam_func2)
+
+    for _ in range(10):
+        prof.runcall(lambda: exam_func1(input=10))
+
+    prof.print_stats(output_unit=1e-6)
+
+
+if __name__ == '__main__':
+    check_profiler()
